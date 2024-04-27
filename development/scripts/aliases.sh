66042fb136f1bcdae5221a412025f3f4ba2fb0dd
Jean-Baptiste Terrazzoni <j2aa@pm.me>
2024-04-27 23:16:46 +0200

    [BUGFIX] Docker script aliases

--- development/scripts/aliases.sh
+++ development/scripts/aliases.sh
@@ -1,3 +1,11 @@
-# Docker
-alias drm="docker rm -vf $(docker ps -aq)"
-alias drmi="docker rmi -f $(docker images -aq)"
+# Docker Processes
+
+drm() {
+    docker rm -vf $(docker ps -aq)
+}
+
+# Docker Images
+
+drmi() {
+    docker rmi -f $(docker images -aq)
+}
